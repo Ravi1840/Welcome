c4a3cf4fd63cf0b484e3509cf5f17fc71fba58d0
Ravi1840 <aedlaravi@gmail.com>
2023-08-28 15:14:29 +0000

    firstResult

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,16 @@
+// Write your code here
+import {Component} from 'react'
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome</h1>
+        <p>Thank you! Happy Learning</p>
+        <button type="button">Subscribe</button>
+      </div>
+    )
+  }
+}
+
+export default Welcome
